5addf6abd43dfd83dc8839fbafc3a9204a0176fd
Veikko Sariola <veikko.sariola@gmail.com>
2018-06-13 21:04:31 +0300

    Implement programmable pipeline to the FMM
    Allows reusing the algorithm for several purposes by changing the input map, output timemap and the callback that is used to evaluate every cell.

--- fastmarching.asm
+++ fastmarching.asm
@@ -1,37 +1,70 @@
 ZP_HEAP_PTR = $FB
-ZP_ARR_TIME = $FD
-ZP_ATIME_1 = $FA
-ZP_ATIME_2 = $F9
-ZP_TMP_1 = $F7
-ZP_TMP_2 = $F5
+ZP_OUTPUT_VEC = $FD
+ZP_INPUT_VEC = $F9
+ZP_ATIME_1 = $F8
+ZP_ATIME_2 = $F7
 FAR_TIME = 150 ; hard maximum for any fast marching time. maximum: 240
 EMPTY_HEAP = 255;
 
 watch heap_location
-watch fmm_time
 watch ZP_HEAP_PTR
-watch ZP_ARR_TIME
+watch ZP_OUTPUT_VEC
+watch ZP_INPUT_VEC
 watch ZP_ATIME_1
 watch ZP_ATIME_2
-
-fmmdebug = 1; defining this includes useful debug commands in the build
 
 ;------------------------------
 ; fmm_reset: resets the fast marching method
 ;------------------------------
 fmm_reset       LDY #0 ; this is the low address byte, but arrival time should be page aligned
-                STY ZP_ARR_TIME
-                LDX #>fmm_time
-                STX ZP_ARR_TIME+1        
+                STY ZP_OUTPUT_VEC
+_fmm_resetpage  LDX #42 ; mutated
+                STX ZP_OUTPUT_VEC+1        
                 LDA #FAR_TIME   
                 LDX #4
-@loop           STA (ZP_ARR_TIME),y 
+@loop           STA (ZP_OUTPUT_VEC),y 
                 INY
                 BNE @loop
-                INC ZP_ARR_TIME+1
+                INC ZP_OUTPUT_VEC+1
                 DEX
                 BNE @loop
                 JMP queue_clear ; tail call into queue_clear
+
+;------------------------------
+; macro fmm_setinput address
+; sets the input map to address (uses only the MSB of the address, because
+; assumes page align)
+; touches A
+;------------------------------
+defm            fmm_setinput
+                LDA #>/1 - heap_location
+                STA _fmm_pshiftin+1
+                endm
+
+;------------------------------
+; macro fmm_setoutput address
+; sets the output  map to address (uses only the MSB of the address, because
+; assumes page align)
+; touches A and carry
+;------------------------------
+defm            fmm_setoutput   
+                LDA #>/1 
+                STA _fmm_resetpage+1
+                SEC
+                SBC #>heap_location
+                STA _fmm_pshiftout+1
+                endm
+
+;------------------------------
+; macro fmm_setcallback address
+; sets the callback function, to allow the user change how a cell is considered
+;-------------------------
+defm            fmm_setcallback
+                LDA #</1
+                STA _fmm_callback+1
+                LDA #>/1
+                STA _fmm_callback+2
+                endm
 
 ;------------------------------
 ; fmm_seed: seeds the fast marching method
@@ -49,21 +82,26 @@ fmm_seed        TYA
 ; fmm_run
 ;------------------------------
 fmm_run         LDA heap_size
-                BEQ _fmm_return  ;  if heap is empty, we're done
+                BEQ fmm_return  ;  if heap is empty, we're done
                 LDA heap_lo
                 SEC
                 SBC #66 ; shift two rows and two columns
                 STA ZP_HEAP_PTR
-                STA ZP_ARR_TIME
+                STA ZP_OUTPUT_VEC
+                STA ZP_INPUT_VEC
                 LDA heap_hi
                 SBC #0  ;
                 STA ZP_HEAP_PTR+1 ; ZP_TMP_A points now to the heapptrs, but shifted two rows / columns
                 CLC
-                ADC #4 ; the arrival times are 4 pages after heap locs
-                STA ZP_ARR_TIME+1 ; ZP_TMP_B points now to arrival times
+_fmm_pshiftout  ADC #42 ; mutated code so the user can choose where to write output
+                STA ZP_OUTPUT_VEC+1 ; ZP_TMP_B points now to arrival times
+                LDA ZP_HEAP_PTR+1
+                CLC
+_fmm_pshiftin   ADC #42 ; mutated code so the user can choose where to read input
+                STA ZP_INPUT_VEC+1
                 LDA heap_priority
                 LDY #66
-                STA (ZP_ARR_TIME),y ; store the priority into the arrival time
+                STA (ZP_OUTPUT_VEC),y ; store the priority into the arrival time
                 JSR queue_deletemin ; this cell is now accepted
                 LDY #65   ; consider the cell on the left
                 JSR _fmm_consider
@@ -74,21 +112,21 @@ fmm_run         LDA heap_size
                 LDY #98    ; consider cell above
                 JSR _fmm_consider
                 JMP fmm_run
-_fmm_return     RTS
+fmm_return     RTS
 
 ;------------------------------
 ; consider a neighbour
 ; Y: index of the cell to be considered, relative to ZP_TMP_* vectors
 ;------------------------------
-_fmm_consider   LDA (ZP_ARR_TIME),y
+_fmm_consider   LDA (ZP_OUTPUT_VEC),y
                 CMP #FAR_TIME
-                BCC _fmm_return ; this cell has already been accepted
+                BCC fmm_return ; this cell has already been accepted
                 DEY  ; shift one left
-                LDA (ZP_ARR_TIME),y
+                LDA (ZP_OUTPUT_VEC),y
                 STA ZP_ATIME_1
                 INY  ; shift two right
                 INY
-                LDA (ZP_ARR_TIME),y
+                LDA (ZP_OUTPUT_VEC),y
                 CMP ZP_ATIME_1
                 BCS @left_le_right
                 STA ZP_ATIME_1 ; ATIME_1 is smaller of the horizontal times
@@ -96,12 +134,12 @@ _fmm_consider   LDA (ZP_ARR_TIME),y
                 SEC
                 SBC #33 ; shift left and down
                 TAY
-                LDA (ZP_ARR_TIME),y
+                LDA (ZP_OUTPUT_VEC),y
                 STA ZP_ATIME_2
                 TYA    
                 ADC #63 ; shift two rows up (2*32), note that carry is set
                 TAY
-                LDA (ZP_ARR_TIME),y
+                LDA (ZP_OUTPUT_VEC),y
                 CMP ZP_ATIME_2
                 BCS @bottom_le_top
                 STA ZP_ATIME_2 ; ATIME_2 is smaller of the vertical times
@@ -113,21 +151,18 @@ _fmm_consider   LDA (ZP_ARR_TIME),y
                 ADC #1 ; carry is guaranteeed to be clear so add 1
                 LDX ZP_ATIME_2
                 STX ZP_ATIME_1
-@ispositive     CMP #15 ; TODO: this is the slowness - where to get?
-                BCC @eikonal
-                LDA #15  ; TODO: again, this is the slowness
-                JMP @pushto_queue
-@eikonal        TAX
-                LDA _fmm_lookup+240,x
-@pushto_queue   CLC
-                ADC ZP_ATIME_1 ; add the smaller of the arrival times to the relative time
-                CMP #FAR_TIME
-                BCS _fmm_return ; the new time is >= FAR_TIME so stop now
-                STA ZP_ATIME_1 ; ATIME_1 is now the new arrival time
+@ispositive     TAX  ; X is now the relative arrive time of the two cells
                 TYA       ; you probably didn't remember that y still is the cell index
                 SEC
                 SBC #32   ; shift one down = go back to the center
                 TAY
+_fmm_callback   JMP $4242 ; mutated to allow the user change the callback
+fmm_continue
+@pushto_queue   CLC
+                ADC ZP_ATIME_1 ; add the smaller of the arrival times to the relative time
+                CMP #FAR_TIME
+                BCS fmm_return ; the new time is >= FAR_TIME so stop now
+                STA ZP_ATIME_1 ; ATIME_1 is now the new arrival time
                 LDA #EMPTY_HEAP
                 CMP (ZP_HEAP_PTR),y
                 BEQ @newcell
@@ -150,70 +185,8 @@ _fmm_consider   LDA (ZP_ARR_TIME),y
                 LDA ZP_ATIME_1 ; A = priority
 @mutate_jmp     JMP 4242 ; tail call into queue_insert or queue_lower_pri      
 
-_fmm_lookup     byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-                byte 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-                byte 1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
-                byte 2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3
-                byte 3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4
-                byte 4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5
-                byte 4,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6
-                byte 5,5,6,6,7,7,7,7,7,7,7,7,7,7,7,7
-                byte 6,6,7,7,7,8,8,8,8,8,8,8,8,8,8,8
-                byte 6,7,7,8,8,8,9,9,9,9,9,9,9,9,9,9
-                byte 7,8,8,8,9,9,9,10,10,10,10,10,10,10,10,10
-                byte 8,8,9,9,10,10,10,10,11,11,11,11,11,11,11,11
-                byte 8,9,9,10,10,11,11,11,11,12,12,12,12,12,12,12
-                byte 9,10,10,11,11,11,12,12,12,13,13,13,13,13,13,13
-                byte 10,10,11,11,12,12,12,13,13,13,14,14,14,14,14,14
-                byte 11,11,12,12,12,13,13,14,14,14,14,15,15,15,15,15
-
 Align
 heap_location   dcb 1024,EMPTY_HEAP ; note: we assume the arrival time is right after the heap
 
-fmm_time        dcb 1024,FAR_TIME
 
 
-;------------------------------
-; 
-; DEBUG related stuff
-; will not be included in the release version 
-; 
-;------------------------------
-
-ifdef fmmdebug
-
-screen_mem = $0400
-
-fmm_dump        LDA #<fmm_time-1
-                STA ZP_TMP_1
-                LDA #>fmm_time-1
-                STA ZP_TMP_1+1
-                LDA #<screen_mem-1
-                STA ZP_TMP_2
-                LDA #>screen_mem-1
-                STA ZP_TMP_2+1
-                LDY #32
-                LDX #25
-@loop           LDA (ZP_TMP_1),y
-                STA (ZP_TMP_2),y
-                DEY
-                BNE @loop
-                LDY #32
-                LDA ZP_TMP_1
-                CLC
-                ADC #32
-                STA ZP_TMP_1
-                LDA ZP_TMP_1+1
-                ADC #0
-                STA ZP_TMP_1+1
-                LDA ZP_TMP_2
-                CLC
-                ADC #40
-                STA ZP_TMP_2
-                LDA ZP_TMP_2+1
-                ADC #0
-                STA ZP_TMP_2+1
-                DEX
-                BNE @loop
-                RTS
-endif
